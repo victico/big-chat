574d4c581ea235ca3ba160bdd295e1dcbb77f7d9
Victor Rivero <VRIVERO@forum.local>
2024-04-14 21:13:50 -0400

    se escapo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <template>
-  <router-view/>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <router-view/>
+
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
